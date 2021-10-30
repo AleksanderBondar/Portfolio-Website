f50bc2d5825dc63e3672d5e1ef206b3166c278f1
AleksanderBondar <86482559+AleksanderBondar@users.noreply.github.com>
2021-10-30 13:52:01 +0000

    Deploying to gh-pages from @ AleksanderBondar/Portfolio-Website@be59b834a3e52b70e8f04cdcbd03b19ac97119df 🚀

---
+++ _next/static/yhYRAYKxrvMkLofJQnMyn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7d0bf13e-a616c8c14870bad0ea3e.js","static/chunks/pages/index-7aafdd8e1f96793a7b42.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
