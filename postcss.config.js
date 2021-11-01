88d730598b0ea03e03aa85dd741644ae94445f4f
Kamqer <86482559+AleksanderBondar@users.noreply.github.com>
2021-11-01 15:01:56 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: [
-        require("tailwindcss")("./tailwind.config.js"),
-        require("autoprefixer"),
-    ],
+    plugins: {
+        tailwindcss: {},
+        tailwindcss: { config: './tailwind.config.js' },
+        autoprefixer: {},
+    },
 };
