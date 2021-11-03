792d52f6c948319d30e7e3348066e0550826fbae
Kamqer <86482559+AleksanderBondar@users.noreply.github.com>
2021-11-03 22:02:23 +0100

    Test

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import { Menu } from '../organism/menu';
 import Calendar from 'react-calendar';
 import { CalendarWrap } from './CalendarWrap';
-
+import 'animate.css';
 
 const WrapperBackground: React.FC = ({ children }) => <div className="bg-black max-w-full pb-14">{children}</div>;
 const WrapperCard: React.FC = ({ children }) => <div className="flex flex-wrap mx-auto justify-left pl-20 pt-20">{children}</div>;
 const WrapperObjects: React.FC = ({ children }) => <div className="flex flex-col">{children}</div>;
-const WrapperTitle: React.FC = ({ children }) => <div className="text-white text-3xl">{children}</div>;
-const WrapperDescription: React.FC = ({ children }) => <div className="text-white text-xl w-8/12">{children}</div>;
+const WrapperTitle: React.FC = ({ children }) => (
+    <div className="text-white text-3xl animate__animated animate__bounce">{children}</div>
+);
+
+const WrapperDescription: React.FC = ({ children }) => (
+    <div className="text-white text-xl w-8/12 animate__animated animate__rotateInUpLeft">{children}</div>
+);
+
 interface HeaderProps {
     isHomePage?: boolean;
 
