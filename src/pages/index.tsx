8887e2e845ba9fe4e12ccb509bbe7880e595fcd5
Kamqer <86482559+AleksanderBondar@users.noreply.github.com>
2021-11-09 14:26:48 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,13 @@ import { Techbar } from '../components/molecules/Rerousel';
 import { Layout } from '../layouts/layout';
 
 
-const PageTitle: React.FC = ({ children }) => <div className="text-5xl text-center mt-4 font-black">{children}</div>;
+const PageTitle: React.FC = ({ children }) => (
+    <div
+        className="text-5xl text-center mt-4 font-black text-[#1d3996]"
+    >
+        {children}
+    </div>
+);
 
 const HomePage = () => {
     return (
